1ff73492189123a1b872c54dc4271398007acfa9
Jones_lund <jonas.lund@hotmail.dk>
2020-03-03 12:48:29 +0100

    added db tables

--- scrapest.py
+++ scrapest.py
@@ -5,29 +5,31 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 import database_connection
 
-option = webdriver.ChromeOptions()
-option.add_argument("-incognito")
+def scrape():
+    option = webdriver.ChromeOptions()
+    option.add_argument("-incognito")
 
-browser = webdriver.Chrome(executable_path="C:/Development/python/chromedriver.exe", options=option)
+    browser = webdriver.Chrome(executable_path="C:/Development/python/chromedriver.exe", options=option)
 
-browser.get("https://ordnet.dk/ddo/ordbog?query=data")
+    browser.get("https://ordnet.dk/ddo/ordbog?query=data")
 
-timeout = 20
+    timeout = 20
 
-try:
-    WebDriverWait(browser, timeout).until(EC.visibility_of_element_located((By.XPATH, "//div[@class='definitionBoxTop']")))
-except TimeoutException:
-    print("timed out")
-    browser.quit()
+    try:
+        WebDriverWait(browser, timeout).until(EC.visibility_of_element_located((By.XPATH, "//div[@class='definitionBoxTop']")))
+    except TimeoutException:
+        print("timed out")
+        browser.quit()
 
-text_elements = browser.find_elements_by_xpath("//div[@class='definitionBoxTop']")
-boejninger = browser.find_elements_by_xpath("//div[@id='id-boj']")
+    text_elements = browser.find_elements_by_xpath("//div[@class='definitionBoxTop']")
+    boejninger = browser.find_elements_by_xpath("//div[@id='id-boj']")
 
-boej = [x.text for x in boejninger]
-text = [x.text for x in text_elements]
+    boej = [x.text for x in boejninger]
+    text = [x.text for x in text_elements]
 
-print("text")
-print(text, "\n", " ", boej)
+    print("text")
+    print(text, "\n", " ", boej)
 
 if __name__ == '__main__':
+    scrape()
     database_connection.DatabaseConnection.create_connection(r"C:\Development\python\scraper\scraperdata.db")
